7817626415515ab1877df5092a17a60ce86dfc1d
maddy1605 <125431026+maddy1605@users.noreply.github.com>
2024-09-28 13:56:43 +0530

    Add files via upload

---
+++ finance tracker app/finance-tracker-app/src/main/java/com/example/finance/tracker/app/FinanceTrackerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.finance.tracker.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceTrackerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceTrackerAppApplication.class, args);
+	}
+
+}
---
+++ finance tracker app/finance-tracker-app/src/test/java/com/example/finance/tracker/app/FinanceTrackerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finance.tracker.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceTrackerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
